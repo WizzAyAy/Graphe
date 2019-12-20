b18c0e29b0c97a57ca7ee0edbf11585a95c48ae5
WizzAyAy <55085753+WizzAyAy@users.noreply.github.com>
2019-12-20 10:04:56 +0100

    Delete question.cc

--- projetPerso/form_psycho/question.cc
+++
@@ -1,9 +0,0 @@
-#include "question.hh"
-
-question::question(const std::string &i, const bool &r)
-    :_intitule(i),_reponseAttendu(r){}
-
-std::ostream &operator<<(std::ostream &os, const question &q){
-    os << q.getNum() << " Question : " << q.getIntitule() << " (" << q.getReponseAttendu() << "->" << q.getReponseEleve() << ")";
-    return os;
-}
